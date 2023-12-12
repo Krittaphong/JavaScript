28a2df14779fe6e0273d33701f86fa758976a8b5
Krittaphong <152238436+Krittaphong@users.noreply.github.com>
2023-12-12 11:34:57 +0700

    HW

--- Lecture03/EX-01.js
+++ Lecture03/EX-01.js
@@ -2,8 +2,13 @@ let value1 = ['Apple', 1, false];
 let value2 = ['Fries', 2, true];
 let value3 = ['Mars', 9, 'Apple'];
 
-// Find common elements
-let commonElements = value1.filter(value => value2.includes(value) || value3.includes(value));
+let commonElements = [];
+
+for (let i = 0; i < value1.length; i++) {
+  if (value2.includes(value1[i]) || value3.includes(value1[i])) {
+    commonElements.push(value1[i]);
+  }
+}
 
 console.log(commonElements);
- || v
+);
