badcf49177dceccb5892fef08c707f10d881ac23
Krittaphong <152238436+Krittaphong@users.noreply.github.com>
2023-12-13 17:58:46 +0700

    Hw

--- Lecture03/Ex-02.js
+++ Lecture03/Ex-02.js
@@ -1,1 +1,8 @@
-const values =['Table' , 'Chairs' , 'Couch'];
+const values = ['Table', 'Chairs', 'Couch'];
+
+for (const item of values) {
+  console.log(`Letters in ${item}:`);
+  for (const letter of item) {
+    console.log(letter);
+  }
+}
