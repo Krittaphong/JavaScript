c3d424683c15d6fd6a5770f6c9d0e00490be5d33
Krittaphong <152238436+Krittaphong@users.noreply.github.com>
2023-12-13 17:54:21 +0700

    Hw

--- Lecture02/Ex-02.js
+++ Lecture02/Ex-02.js
@@ -1,18 +1,24 @@
 function equation(str) {
     let spit = str.split("");
-    let letter = "";
+    let strletter = "";
+    let letter = [];
     let operater = "";
     let sum = 0;
-
+    
     for (let i = 0 ; i < spit.length ; i++) {
         if (isNaN(spit[i])) {
             operater += spit[i];
+            letter.push(strletter);
+            strletter = "";
         }
         else {
-            letter += spit[i];
+            strletter += spit[i];
+            if (i == spit.length - 1) {
+                letter.push(strletter);
+            }
         }
     }
-
+    
     for (let i = 0 ; i < spit.length ; i++) {
         if (operater[i] == "+") {
             if (i == 0) {
@@ -32,18 +38,18 @@ function equation(str) {
             }
         }
 
-        else if (operater[i] == "") {
+        else if (operater[i] == "*") {
             if (i == 0) {
-                sum = parseInt(letter[i]) + parseInt(letter[i+1]);
+                sum = parseInt(letter[i]) * parseInt(letter[i+1]);
             }
             else{
-                sum = parseInt(letter[i+1]);
+                sum *= parseInt(letter[i+1]);
             }
         }
     }
     console.log(sum)
 }
 
-equation("1+1");
-equation("74-2");
+equation("11+1+2");
+equation("10*4");
 equation("1+1+1+1+1");
